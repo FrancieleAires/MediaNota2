48657433aaee8c56b32f647bb0df1d020a396bae
FrancieleAires <f.aires2016@gmail.com>
2023-01-18 08:30:05 -0300

    Adicionar arquivos de projeto.

---
+++ MediaNota2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProjetoBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, media;
+
+
+            nota1 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            nota2 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            nota3 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = (((nota1 * 2) + (nota2 * 3) + (nota3 * 5))) / 10;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
